966a19eaa3b58166fe5d2d166d67d818dcd55612
Agustin F. Ruiz <62730262+darksoul96@users.noreply.github.com>
2022-06-25 00:34:31 -0300

    Creation of 'club' project.

---
+++ Core/src/main/java/configuration/BeanConfig.java
@@ -0,0 +1,62 @@
+package configuration;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class /*HibernateJpaAutoConfiguration.class*/})
+@EnableTransactionManagement
+@ComponentScan("configuration")
+public class BeanConfig {
+
+//    @Bean
+//    public LocalSessionFactoryBean sessionFactory() {
+//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+//        sessionFactory.setDataSource(dataSource());
+//        sessionFactory.setPackagesToScan(
+//                "com.mvc.spring_demo_mvc" );
+//        sessionFactory.setHibernateProperties(hibernateProperties());
+//
+//        return sessionFactory;
+//    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://server.milanesa-chan.com:5432/club");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("estanislao");
+
+        return dataSource;
+    }
+
+//    @Bean
+//    public PlatformTransactionManager hibernateTransactionManager() {
+//        HibernateTransactionManager transactionManager
+//                = new HibernateTransactionManager();
+//        transactionManager.setSessionFactory(sessionFactory().getObject());
+//        return transactionManager;
+//    }
+//
+//    private final Properties hibernateProperties() {
+//        Properties hibernateProperties = new Properties();
+//        hibernateProperties.setProperty(
+//                "hibernate.hbm2ddl.auto", "create-drop");
+//        hibernateProperties.setProperty(
+//                "hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+//
+//        return hibernateProperties;
+//    }
+
+
+}
