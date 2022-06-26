6e998af75f889bd78b296fabf54a2e4e4973dea8
Sebastian Bernal <Sbernal1995@gmail.com>
2022-06-25 21:37:08 -0300

    Add grupo familiar + socio working

--- Core/src/main/java/controllers/ClientController.java
+++ Core/src/main/java/controllers/ClientController.java
@@ -6,15 +6,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import persistence.dao.ISocioDAO;
 import persistence.entities.GrupoFamiliar;
 import persistence.entities.Socio;
 import services.interfaces.IGrupoFamiliarService;
+import services.interfaces.ISocioService;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 
 @Controller
 public class ClientController {
 
     @Autowired
     private IGrupoFamiliarService grupoFamiliarService;
+    @Autowired
+    private ISocioService socioService;
 
     @RequestMapping(value = "/newClient", method = RequestMethod.GET)
     public String selectUserType () {
@@ -34,13 +43,14 @@ public class ClientController {
     @RequestMapping(value = "/newClient/group/save", method = RequestMethod.POST)
     public String saveGroup(@RequestParam(name="domicilio") String domicilio, @RequestParam(name="tel_fijo") String telefono,
                             @RequestParam(name="nombre") String nombre, @RequestParam(name="apellido") String apellido,
-                            @RequestParam(name="celular") String celular, @RequestParam(name="fecha_nac") String fecha_nac,
-                            @RequestParam(name="tipo_socio") char tipo) {
+                            @RequestParam(name="celular") String celular, @RequestParam(name="fecha_nac") String fecha_nac) throws ParseException {
 
-        //Guarda Grupo Familiar
         GrupoFamiliar grupoFamiliar = new GrupoFamiliar(domicilio,telefono);
+        Date fecha_nacimiento = new SimpleDateFormat("dd/MM/yyyy").parse(fecha_nac);
+        Date fecha_inscripcion_club = Date.from(Instant.now());
         int cod_base = grupoFamiliarService.save(grupoFamiliar);
-        //Guarda el Socio titular
+        Socio socio = new Socio(cod_base,nombre,apellido,celular,fecha_nacimiento,fecha_inscripcion_club,'M');
+        int id_socio = socioService.save(socio);
         return "/clients/new_group";
     }
 }
